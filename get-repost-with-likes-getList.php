a4250c8566351a1d8138b3326a7a0dd5d8645579
pligin <i@psweb.ru>
2018-05-23 14:32:42 +0300

    get.Repost with likes.getList

---
+++ get-repost-with-likes-getList.php
@@ -0,0 +1,25 @@
+<?php
+/* 
+ * Author: pligin.
+ * Author email: i@psweb.ru | i@psweb.by
+ * Author Site URL: https://psweb.ru | https://psweb.by
+ */
+
+require ('classes/vkapi.class.php');
+$api_id = '1111111'; //ID приложения | ID APP
+$secret_key = 'secret_key'; // секретный ключ | Secret Key
+$owner_id = '-11111111'; //идентификатор владельца Like-объекта: id пользователя, id сообщества (со знаком «минус») или id приложения.
+$post_id = '11111';//идентификатор поста. в ссылке на пост крайние цифры после знака "_"
+
+$VK = new vkapi($api_id, $secret_key);//создаем объект класса vkapi | create an object of class vkapi
+$resp = $VK->api('likes.getList',//используем метод likes.getList
+        array(
+            'type' => 'post',// тип объекта - запись
+            'owner_id' => $owner_id,
+            'item_id' => $post_id,
+            'filter' => 'copies',//copies — возвращать информацию только о пользователях, рассказавших об объекте друзьям
+            'extended' => '1',//1 — возвращать расширенную информацию о пользователях и сообществах
+            'count' => '1000'//количество возвращаемых идентификаторов пользователей
+            )
+        );
+print_r($resp);
